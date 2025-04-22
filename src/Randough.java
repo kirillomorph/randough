2752d0209adc96c3fd5d1f4f388d7fc6a9708c96
kirillomorph <kirillomorph@gmail.com>
2025-04-22 17:34:53 +0000

    Fixed bugs, almost optimized

--- src/Randough.java
+++ src/Randough.java
@@ -1,43 +1,32 @@
 import java.util.*;
 
+//import javax.sound.sampled.SourceDataLine;
+
 public class Randough {
-
- /* public static double Random () {
-        double random = Math.random();
-
-        while (random > 100 || random < 0 || random % 10 != 0) {
-            random = Math.random();
-        }
-        return random;
-    }*/ 
-    
-    public static int Random () {
+    public static void main(String[] args) {
         Random rand = new Random();
         int min = 0;
         int max = 100;
         int random = (rand.nextInt(max - min + 1) + min);
 
-        return random;
-    }
+        System.out.print ("Guess the number between 0 and 100: ");
 
+        Scanner sc = new Scanner(System.in);
+        //int input = sc.nextInt();
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
+        while (true) {
+            int input = sc.nextInt();
 
-        System.out.print("Guess the number from 0 to 100: ");
-        int input = sc.nextInt();
-        if (input > Random()) {
-            System.out.println("Too much... Try again: ");
-            input = sc.nextInt();
+            if (random > input) {
+                System.out.print("Not enough... Try again: ");
+            } else if (random < input) {
+                System.out.print("Too much... Try again: ");
+            } else {
+                System.out.print("You won!!!");
+                break;
+            }
         }
-        if (input < Random()) {
-            System.out.println("Not enough... Try again: ");
-            input = sc.nextInt();
-        }
-        if (input == Random()) {
-            System.out.println("You won!!!");
-            System.out.println("The number was: " + Random());
-        }
+
         sc.close();
     }
 }
